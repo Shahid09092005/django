c4b170c9102b4d6b629c20ca810fab7013b9f2e4
Shahid09092005 <shahidmansuri4444@gmail.com>
2026-05-10 16:10:20 +0530

    form working correctly

--- vege/views.py
+++ vege/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Receipe
+
 
 # Create your views here.
 def receipies(request):
-    return render(request, 'receipies.html')
+    if request.method == 'POST':
+        data= request.POST
+        receipe_name = data.get('receipe_name')
+        receipe_description = data.get('receipe_description')
+        receipe_image = request.FILES.get('receipe_image')
+        Receipe.objects.create(
+            receipe_name = receipe_name,
+            receipe_description = receipe_description,
+            receipe_image = receipe_image
+        )
+        
+        return render(request, 'receipies.html')
+    return render(request, 'receipies.html') 
